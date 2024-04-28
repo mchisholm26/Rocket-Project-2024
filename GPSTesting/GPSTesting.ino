70ecaa9e476c9157943075dfdca2aef70defe034
Mac-Coleman <williamcormaccoleman@gmail.com>
2024-04-28 18:07:12 -0500

    Added delay and removed unneeded code...
    Sometimes works and sometimes doesn't. Seems random.

--- GPSTesting/GPSTesting.ino
+++ GPSTesting/GPSTesting.ino
@@ -11,122 +11,16 @@ const int gps_freq = 5500000;
 const int gps_spi_mode = SPI_MODE0;
 const int gps_bit_order = MSBFIRST;
 
-char poll_position_message[] = "PUBX,00";
-char poll_satellites_message[] = "PUBX,03";
-char output_rate_message[] = "PUBX,40,RMC,1,1,1,1,2,0";
-const char* nmea_stop_code = "\r\n";
-
 void setup_gps() {
   pinMode(gps_select, OUTPUT);
   pinMode(gps_reset, OUTPUT);
   digitalWrite(gps_select, HIGH);
 
   digitalWrite(gps_reset, LOW);
-  delay(10);
+  delay(125);
   digitalWrite(gps_reset, HIGH);
-}
 
-uint8_t read_byte_from_gps() {
-  SPI.beginTransaction(SPISettings(gps_freq, gps_bit_order, gps_spi_mode));
-  digitalWrite(gps_select, LOW);
-  uint8_t b = SPI.transfer(255);
-  digitalWrite(gps_select, HIGH);
-  SPI.endTransaction();
-  return b;
-}
-
-void send_message_to_gps(char* message) {
-  int len = strlen(message);
-
-  Serial.println("Sending message:");
-
-  uint8_t checksum = 0;
-
-  SPI.beginTransaction(SPISettings(gps_freq, gps_bit_order, gps_spi_mode));
-  digitalWrite(gps_select, LOW);
-
-  SPI.transfer((uint8_t) "$");
-  Serial.print("$");
-
-  for (int i = 0; i<len; i++) {
-    SPI.transfer(message[i]);
-    Serial.printf("%c", message[i]);
-    checksum ^= message[i];
-  }
-
-  SPI.transfer((uint8_t) "*");
-  Serial.print("*");
-
-  char cs[6];
-  snprintf(cs, 5, "%02x\r\n", checksum);
- 
-  int l = strlen(cs);
-
-  for (int i = 0; i < l; i++) {
-    SPI.transfer(cs[i]);
-    Serial.printf("%c", cs[i]);
-  }
-
-  digitalWrite(gps_select, HIGH);
-  SPI.endTransaction();
-}
-
-void receive_message_from_gps(char* buffer, int len) {
-  memset(buffer, 0, len);
-
-  int place = 0;
-
-  Serial.println("Receiving message:");
-  
-  SPI.beginTransaction(SPISettings(gps_freq, gps_bit_order, gps_spi_mode));
-  digitalWrite(gps_select, LOW);
-
-  //Read bytes as long as they are available and a message end has not been received. (0x0A)
-  while (place < len && buffer[place-1] != 0x0A) {
-    uint8_t b = SPI.transfer(0xFF);
-    if (b == 0xFF) { //Try again if we overran the buffer.
-      delay(10);
-      Serial.println("Overran");
-      continue;
-    }
-    buffer[place] = b;
-    Serial.printf("%02x ", b);
-    place += 1;
-  }
-
-  digitalWrite(gps_select, HIGH);
-  SPI.endTransaction();
-}
-
-void ubx_checksum(uint8_t buffer[], int len, uint8_t* ck_a, uint8_t* ck_b) {
-  *ck_a = 0;
-  *ck_b = 0;
-  for (int i = 0; i < len; i++) {
-    *ck_a = *ck_a + buffer[i];
-    *ck_b = *ck_b + *ck_a;
-  }
-}
-
-void send_ubx_message(uint8_t message[], int len) {
-  uint8_t ck_a;
-  uint8_t ck_b;
-  ubx_checksum(message, len, &ck_a, &ck_b);
-
-  SPI.beginTransaction(SPISettings(gps_freq, gps_bit_order, gps_spi_mode));
-  digitalWrite(gps_select, LOW);
-
-  SPI.transfer(0xb5);
-  SPI.transfer(0x62);
-
-  for (int i = 0; i<len; i++) {
-    SPI.transfer(message[i]);
-  }
-
-  SPI.transfer(ck_a);
-  SPI.transfer(ck_b);
-
-  digitalWrite(gps_select, HIGH);
-  SPI.endTransaction();
+  delay(700);
 }
 
 void setup() {
@@ -141,6 +35,7 @@ void setup() {
   Serial.println("Starting...");
 
   setup_gps();
+  Serial.println("GPS setup.");
 }
 
 void loop() {
@@ -149,11 +44,16 @@ void loop() {
   SPI.beginTransaction(SPISettings(gps_freq, gps_bit_order, gps_spi_mode));
   digitalWrite(gps_select, LOW);
 
-  delay(10);
+  delay(1);
 
   uint8_t b = SPI.transfer(0xFF);
-  Serial.printf("%c", b);
 
   digitalWrite(gps_select, HIGH);
   SPI.endTransaction();
+
+  if (b == 0xFF) {
+    return;
+  }
+
+  Serial.printf("%c", b);
 }
